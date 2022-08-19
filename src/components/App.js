ffa7369ddf9c6903053e764bce571ea657f556d2
jroiceee <jroicealdeza@gmail.com>
2022-08-19 13:23:03 +0800

    customize bootstrap themes

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-
-import './App.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
-      
+      <p>Hello bruv</p>
+      <Button variant='primary'>Nah</Button>
     </div>
   );
 }
