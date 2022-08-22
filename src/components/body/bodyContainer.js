54d85740be98a9e84f8b928497c612fef9ca911e
jroiceee <jroicealdeza@gmail.com>
2022-08-22 14:14:37 +0800

    change state to object

--- src/components/body/bodyContainer.js
+++ src/components/body/bodyContainer.js
@@ -3,47 +3,32 @@ import Container from "react-bootstrap/esm/Container";
 import GeneralInformationForm from "./form-components/generalInformation";
 import Header from "./header";
 import { useState } from "react";
-import SchoolInformationForm from "./form-components/schoolInformationForm";
+//import SchoolInformationForm from "./form-components/schoolInformationForm";
 
 function BodyContainer() {
-  const [firstName, setFirstName] = useState("");
-  const [middleInitial, setMiddleInitial] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [phoneNumber, setPhoneNumber] = useState("");
-  const [course, setCourse] = useState("");
-  const [yearLevel, setYearLevel] = useState("");
-  const [lrn, setLrn] = useState("");
-  const [schoolName, setSchoolName] = useState("Roice University");
+  let defaultStudentInformation = {
+    name: "",
+    email: "",
+    city: "",
+    province: "",
+    course: "",
+    yearLevel: "",
+    lrn: "",
+    schoolName: "Roice University",
+  };
+  const [studentInfo, setStudentInfo] = useState(defaultStudentInformation);
 
   return (
     <Container className="mt-2">
       <Header />
       <GeneralInformationForm
-        firstName={firstName}
-        middleInitial={middleInitial}
-        lastName={lastName}
-        email={email}
-        phoneNumber={phoneNumber}
-        course={course}
-        setFirstName={setFirstName}
-        setMiddleInitial={setMiddleInitial}
-        setLastName={setLastName}
-        setEmail={setEmail}
-        setPhoneNumber={setPhoneNumber}
-        setCourse={setCourse}
+        studentInfo={studentInfo}
+        setStudentInfo={setStudentInfo}
       />
-      <SchoolInformationForm 
-      course={course} 
-      yearLevel={yearLevel}
-      lrn={lrn}
-      requiredLrnLength={12}
-      schoolName={schoolName}
-      setCourse={setCourse}
-      setYearLevel={setYearLevel}
-      setLrn={setLrn}
-      setSchoolName={setSchoolName}
-      />
+      {/* <SchoolInformationForm
+        studentInfo={studentInfo}
+        setStudentInfo={setStudentInfo}
+      /> */}
     </Container>
   );
 }
--- src/components/body/form-components/generalInformation.js
+++ src/components/body/form-components/generalInformation.js
@@ -1,48 +1,33 @@
 import Form from "react-bootstrap/Form";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import React, { useCallback } from "react";
 import "react-phone-number-input/style.css";
 import {
   debouncedValidateRequiredInput,
-  debouncedValidateInput,
   debouncedValidateEmail,
+  debouncedValidateInput,
 } from "./validation";
-import PhoneInput from "react-phone-number-input";
 import "../../../styles/form-customize.css";
 
-function GeneralInformationForm({
-  firstName,
-  middleInitial,
-  lastName,
-  email,
-  phoneNumber,
-  setFirstName,
-  setMiddleInitial,
-  setLastName,
-  setEmail,
-  setPhoneNumber,
-}) {
+function GeneralInformationForm({ studentInfo, setStudentInfo }) {
   const changeInputHandler = useCallback((e) => {
-    const value = e.target.value;
-    const targetID = e.target.id;
-    switch (targetID) {
-      case "formFirstName":
-        setFirstName(value);
-        debouncedValidateRequiredInput(e, "first-name-error");
+    const { id, value } = e.target;
+    setStudentInfo((prevState) => ({
+      ...prevState,
+      [id]: value,
+    }));
+    console.log(id + "   " + value);
+    switch (id) {
+      case "name":
+        debouncedValidateRequiredInput(e, "name-error");
         break;
-      case "formMiddleInitial":
-        setMiddleInitial(value);
-        debouncedValidateInput(e, "middle-initial-error");
-        break;
-      case "formLastName":
-        setLastName(value);
-        debouncedValidateRequiredInput(e, "last-name-error");
-        break;
-      case "formBasicEmail":
-        setEmail(value);
+      case "email":
         debouncedValidateEmail(e, "email-error");
         break;
-      case "formPhoneNumber":
-        setPhoneNumber(value);
+      case "city":
+      case "province":
+        debouncedValidateInput(e, "city-error");
         break;
     }
   }, []);
@@ -50,61 +35,50 @@ function GeneralInformationForm({
   return (
     <Form className="mt-4">
       <h6>General Information</h6>
-      <Form.Group className="mb-3" controlId="formFirstName">
-        <Form.Label>First Name (required)</Form.Label>
+      <Form.Group className="mb-2" controlId="name">
         <Form.Control
           onChange={changeInputHandler}
-          value={firstName}
+          value={studentInfo.name}
           type="text"
-          placeholder="Text here"
+          placeholder="Full Name (required)"
         />
-        <div id="first-name-error" className="error"></div>
+        <div id="name-error" className="error"></div>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formMiddleInitial">
-        <Form.Label>Middle Initial</Form.Label>
+      <Form.Group className="mb-2" controlId="email">
         <Form.Control
           onChange={changeInputHandler}
-          value={middleInitial}
-          type="text"
-          placeholder="Text here"
-        />
-        <div id="middle-initial-error" className="error"></div>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formLastName">
-        <Form.Label>Last Name (required)</Form.Label>
-        <Form.Control
-          onChange={changeInputHandler}
-          value={lastName}
-          type="text"
-          placeholder="Text here"
-        />
-        <div id="last-name-error" className="error"></div>
-      </Form.Group>
-
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address (required)</Form.Label>
-        <Form.Control
-          onChange={changeInputHandler}
-          value={email}
+          value={studentInfo.email}
           type="email"
-          placeholder="Enter email"
+          placeholder="Email (required)"
         />
         <div id="email-error" className="error"></div>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formPhoneNumber">
-        <Form.Label>Phone Number</Form.Label>
-        <span className="mx-3">{phoneNumber}</span>
-        <PhoneInput
-          onChange={setPhoneNumber}
-          value={phoneNumber}
-          defaultCountry="PH"
-          type="text"
-          placeholder="xxx xxx xxxx"
-        />
-      </Form.Group>
+      <Row>
+        <Col>
+          <Form.Group className="mb-2" controlId="city">
+            <Form.Control
+              onChange={changeInputHandler}
+              value={studentInfo.city}
+              type="text"
+              placeholder="City/Municipality"
+            />
+            <div id="city-error" className="error"></div>
+          </Form.Group>
+        </Col>
+        <Col>
+          <Form.Group className="mb-2" controlId="province">
+            <Form.Control
+              onChange={changeInputHandler}
+              value={studentInfo.province}
+              type="text"
+              placeholder="State/Province"
+            />
+            <div id="province-error" className="error"></div>
+          </Form.Group>
+        </Col>
+      </Row>
     </Form>
   );
 }
